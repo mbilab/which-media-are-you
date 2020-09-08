f3486ac7d85873f9b66c4c667b98c62602c823f4
tintin327 <tintin890327@gmail.com>
2020-09-08 20:56:44 +0800

    pug,vue files

---
+++ app/app.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+// Step 1 code here
+import 'semantic-ui-offline/semantic.min.css'
+import App from './App.vue'
+
+new Vue({
+	  el: '#app',
+	  render: h => h(App)
+})
+/* 
+*/
