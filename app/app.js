5f4502b3a824949f097e98fa0a44cb7a8703f8d2
tintin327 <tintin890327@gmail.com>
2020-09-11 17:13:22 +0800

    update app.js

--- app/app.js
+++ app/app.js
@@ -1,11 +1,7 @@
 import Vue from 'vue'
-// Step 1 code here
-import 'semantic-ui-offline/semantic.min.css'
 import App from './App.vue'
 
 new Vue({
 	  el: '#app',
 	  render: h => h(App)
 })
-/* 
-*/
